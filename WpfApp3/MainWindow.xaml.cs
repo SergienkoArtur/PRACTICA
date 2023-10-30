574210739916a56c7c5863328a5377b4785b2902
lucienmoore <vladislavpolyarnyj@gmail.com>
2023-10-30 14:34:44 +0400

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using static WpfApp3.MainWindow;
+
+namespace WpfApp3
+{
+    public class user
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<transport> transports { get; set; }
+
+        public DbSet<user> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = UsersDb; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
+        }
+    }
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void vhod_Click(object sender, RoutedEventArgs e)
+        {
+            var login = log.Text;
+            var password = passw.Text;
+
+            var context = new AppDbContext();
+
+            var user_exists = context.Users.SingleOrDefault(x => x.Login == login && x.Password == password);
+
+            if(user_exists is not null)
+            {
+                MessageBox.Show("Это регистрация а не авторизация критин");
+                return;
+            }
+
+            var user = new user { Login = login, Password = password };
+
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+
+        private void avt_Click(object sender, RoutedEventArgs e)
+        {
+            var login = avt_log.Text;
+            var password = avt_passw.Text;
+
+            var context = new AppDbContext();
+
+            var user = context.Users.SingleOrDefault(x => x.Login == login && x.Password == password);
+            if(user is null)
+            {
+                MessageBox.Show("Неправильный логин или пароль, i think что тебе нужно пробовать еще");
+                return;
+            }
+
+            MessageBox.Show("Вы успешно вошли в аккаунт");
+        }
+
+        public class transport
+        {
+            public int ID { get; set; }
+
+            public user Owner { get; set; }
+
+            public string Identifier { get; set; }
+        }
+}
+}
---
+++ WpfApp3/Migrations/20231028112045_Inital.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfApp3.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inital : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transports",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerId = table.Column<int>(type: "int", nullable: false),
+                    Identifier = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transports", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_transports_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transports_OwnerId",
+                table: "transports",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "transports");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
